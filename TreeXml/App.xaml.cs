f6d5dfaf08e49ea12af9a36e327ad65548661533
TDenis <dtimukhin@yandex.ru>
2017-09-14 00:31:51 +0300

    Add project files.

---
+++ TreeXml/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace TreeXml
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
